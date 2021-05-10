41da215e1ff5c6f04701d3faa05c5eab383daf3e
Andres Sequeda Higuita <80718240+andressequeda@users.noreply.github.com>
2021-05-09 22:06:36 -0500

    Add files via upload

---
+++ ejercicio9.py
@@ -0,0 +1,4 @@
+numero1  =  int ( input ( "Ingrese el primer número:" ))
+numero2  =  int ( input ( "Ingrese el segundo número:" ))
+numero3  =  int ( input ( "Ingrese el tercer número:" ))
+print ( "El promedio de los tres números es:" , ( numero1  +  numero2  +  numero3 ) /  3 )
